05bf971043ded4dcc304ce7380a48eb398380687
Gary Woodfine <gary.woodfine@threenine.co.uk>
2021-03-26 17:03:24 +0000

    tidy up directory structure

---
+++ src/Api/Endpoints/Article/Response/ArticleResponse.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Api.Endpoints.Article.Response
+{
+    public class ArticleResponse
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Summary { get; set; }
+        public string SubHeading { get; set; }
+        public string Content { get; set; }
+        public DateTime Published { get; set; }
+    }
+}
