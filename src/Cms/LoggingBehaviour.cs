51d9e3d69965f0fdd9cf384692aec26fe33bb12d
Gary Woodfine <gary.woodfine@threenine.co.uk>
2021-05-23 19:44:34 +0100

    finished logging pipeline behaviour

---
+++ src/Cms/LoggingBehaviour.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace Cms
+{
+    public class LoggingBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    {
+        private readonly ILogger<LoggingBehaviour<TRequest, TResponse>> _logger;
+        public LoggingBehaviour(ILogger<LoggingBehaviour<TRequest, TResponse>> logger)
+        {
+            _logger = logger;
+        }
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            //Request
+            _logger.LogInformation($"Handling {typeof(TRequest).Name}");
+            Type myType = request.GetType();
+            IList<PropertyInfo> props = new List<PropertyInfo>(myType.GetProperties());
+            foreach (PropertyInfo prop in props)
+            {
+                object propValue = prop.GetValue(request, null);
+                _logger.LogInformation("{Property} : {@Value}", prop.Name, propValue);
+            }
+            var response = await next();
+            //Response
+            _logger.LogInformation($"Handled {typeof(TResponse).Name}");
+            return response;
+        }
+    }
+}
