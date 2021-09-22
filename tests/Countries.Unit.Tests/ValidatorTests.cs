e0a52f20aeda1b9a3903b023b3d15877476a77b4
Gary Woodfine <gary.woodfine@threenine.co.uk>
2021-09-22 19:41:32 +0100

    Worlbank Country Codes

---
+++ tests/Countries.Unit.Tests/ValidatorTests.cs
@@ -0,0 +1,28 @@
+using Boleyn.Countries.Activities.Sample.Get;
+using FluentValidation.TestHelper;
+using Xunit;
+
+namespace Countries.Unit.Tests
+{
+    public class ValidatorTests
+    {
+        private readonly Validator _validator;
+
+        public ValidatorTests()
+        {
+            _validator = new Validator();
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("somelongcode")]
+        [InlineData("a")]
+        [InlineData("123")]
+        public void Should_have_validation_error_for_empty_iso_code(string input)
+        {
+            var query = new Query();
+            var result = _validator.TestValidate(query);
+            result.ShouldHaveValidationErrorFor(x => x.CountryCode);
+        }
+    }
+}
