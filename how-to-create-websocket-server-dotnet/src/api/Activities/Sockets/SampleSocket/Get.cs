0033b2b01c32cb273ff7cf0524a084fd6c712801
Gary Woodfine <lnb0l9dc@duck.com>
2023-09-10 10:36:44 +0100

    working websoket

---
+++ how-to-create-websocket-server-dotnet/src/api/Activities/Sockets/SampleSocket/Get.cs
@@ -0,0 +1,61 @@
+using System.Net;
+using System.Net.WebSockets;
+using System.Text;
+using Ardalis.ApiEndpoints;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace Api.Activities.Sockets.Queries.SampleSocket;
+
+[Route("ws")]
+public class WebSocketQuery : EndpointBaseAsync.WithoutRequest.WithoutResult
+{
+    private readonly ILogger<WebSocketQuery> _logger;
+    public WebSocketQuery(ILogger<WebSocketQuery> logger)
+    {
+        _logger = logger;
+    }
+        
+    [HttpGet]
+    [SwaggerOperation(
+        Summary = "Web Socket Upgrade Request to initiate a Web Socket Session",
+        Description = "Web Socket Upgrade Request to initiate a Web Socket Session",
+        OperationId = "1318F7F5-4FE0-4315-BBF3-0DD1A9CABB5C",
+        Tags = new[] { Routes.Sockets})
+    ]
+    [ProducesErrorResponseType(typeof(BadRequestObjectResult))]
+    public override  async Task HandleAsync(CancellationToken cancellationToken = new CancellationToken())
+    {
+        //let ws = new WebSocket('wss://localhost:5001/ws');
+        if (HttpContext.WebSockets.IsWebSocketRequest)
+        {
+            using var webSocket = await HttpContext.WebSockets.AcceptWebSocketAsync();
+            _logger.Log(LogLevel.Information, "WebSocket connection established");
+            await Echo(webSocket);
+        }
+        else
+        {
+            HttpContext.Response.StatusCode = (int) HttpStatusCode.BadRequest;
+        }
+    }
+    private async Task Echo(WebSocket webSocket)
+    {
+        var buffer = new byte[1024 * 4];
+        var result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+        _logger.Log(LogLevel.Information, "Message received from Client");
+
+        while (!result.CloseStatus.HasValue)
+        {
+            var serverMsg = Encoding.UTF8.GetBytes($"Client Message: {Encoding.UTF8.GetString(buffer)}");
+            await webSocket.SendAsync(new ArraySegment<byte>(serverMsg, 0, serverMsg.Length), result.MessageType, result.EndOfMessage, CancellationToken.None);
+            _logger.Log(LogLevel.Information, "Message sent to Client");
+
+            buffer = new byte[1024 * 4];
+            result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            _logger.Log(LogLevel.Information, "Message received from Client");
+        }
+
+        await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
+        _logger.Log(LogLevel.Information, "WebSocket connection closed");
+    }
+}
